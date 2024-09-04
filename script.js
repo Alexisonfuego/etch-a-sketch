0be7814526b36a42bc7348110f12e8bca3a686f9
Alexisonfuego <29289359+Alexisonfuego@users.noreply.github.com>
2024-09-04 23:20:57 +0900

    fixed grid creation function

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 let sketchPad = document.querySelector('.container')
 
 
-let createGrid = function(size) {
-    // let size = prompt("What size grid would you like?", "Enter a number between 2 and 100")
+let createGrid = function() {
+    let size = prompt("What size grid would you like?", "Enter a number between 2 and 100")
     sketchPad.innerHTML = '';
 
     for (let i = 0; i < size * size; i++) {
         let cell = document.createElement('div');
+        // set size of cells based on user input
+        cell.style.width = `calc(100% / ${size})`;
+        cell.style.height = `calc(100% / ${size})`;
         sketchPad.appendChild(cell);
     }
 }
 
-createGrid(32);
+createGrid();
